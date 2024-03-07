9d4a91df9825916f70ba97a40f82d3c862bfc875
TamNhien <canhthien91@gmail.com>
2024-03-07 19:57:42 +0700

    Add project files.

---
+++ LTCB_TinhGiaTriBieuThucS.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    double S = 0.0;
+
+    cout << "Nhap so nguyen duong n : ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) S += 1.0 / (i * i * i);
+
+    cout << fixed << setprecision(5) << "Gia tri cua bieu thuc S la : " << S << endl;
+
+    return 0;
+}
